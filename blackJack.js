f9a27b388cc537518c93e8bc117d6767ee7c3a5c
Santiago Sierra <santiagosierrak10@gmail.com>
2022-07-11 23:01:14 -0500

    Created Card and CardsDeck classes along with DeckCreation method

--- blackJack.js
+++ blackJack.js
@@ -1,8 +1,61 @@
+
+
 const readLine = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-readLine.question('This is the question\n', (userInput) => {
-    console.log(userInput);
-});
+function getUserInput(userPrompt){
+    readLine.question(userPrompt + "\n", (userInput) => {
+        return userInput;
+    });
+}
+
+
+
+
+
+class Card {
+    constructor(value, suit){
+        this.value = value;
+        this.suit = suit;
+        
+    }
+}
+
+
+class CardsDeck{
+    constructor(){
+        this.cards = [];
+    }
+
+
+    createNewDeck(){
+        const values = ["A","2","3","4","5","6", "7", "8", "9", "10", "J", "Q", "K"];
+        const suits = ["S","H","C","D"];
+    
+    
+        for(let i = 0; i < values.length; i++){
+            for(let j = 0; j < suits.length; j++){
+                let card = new Card(values[i] + "-" + suits[j]);
+                this.cards.push(card)
+            }
+        }
+    
+    }
+
+    
+
+}
+
+
+
+let deck1 = new CardsDeck();
+deck1.createNewDeck();
+
+console.log(deck1.cards);
+
+
+
+
+
